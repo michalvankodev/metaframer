10f624dc5a4778a7029dc185dcf0aa99559caf9f
Michal Vanko <mklakakilli@gmail.com>
2024-05-17 23:41:15 +0200

    Initialize cargo project

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // let pattern = std::env::args().nth(1).expect("no pattern given");
+    // let path = std::env::args().nth(2).expect("no path given");
+
+    println!(
+        "This is metaframer! A CLI tool to generate frames for images from their metadata to be displayed alongside images in videos or other scenarios.",
+    )
+}
