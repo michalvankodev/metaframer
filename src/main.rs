b52c9230c0cecb81207a54089503cb3f2764332a
Michal Vanko <mklakakilli@gmail.com>
2024-05-19 21:09:54 +0200

    Read file and dump exif data

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
+use clap::Parser;
+use std::{
+    fs::File,
+    io::{BufReader, Error},
+};
+
+#[derive(Parser)]
+struct CliArgs {
+    path: std::path::PathBuf,
+}
+
 fn main() {
     // let pattern = std::env::args().nth(1).expect("no pattern given");
     // let path = std::env::args().nth(2).expect("no path given");
 
-    println!(
-        "This is metaframer! A CLI tool to generate frames for images from their metadata to be displayed alongside images in videos or other scenarios.",
-    )
+    let args = CliArgs::parse();
+
+    println!("File: {:?}", args.path);
+
+    let file = File::open(args.path).unwrap();
+    let mut bufreader = BufReader::new(&file);
+    let exifreader = exif::Reader::new();
+    let exif = exifreader.read_from_container(&mut bufreader).unwrap();
+    for f in exif.fields() {
+        println!(
+            "{} {} {}",
+            f.tag,
+            f.ifd_num,
+            f.display_value().with_unit(&exif)
+        );
+    }
+
+    ()
 }
